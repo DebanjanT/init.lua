e2dcd3b2b50a2a2c7e36af37a5b9241b09f0bb67
Debanjan Tewary <89004599+DebanjanT@users.noreply.github.com>
2024-05-11 08:40:47 +0530

    my lua not prime lua

--- lua/theprimeagen/lazy/toggleterm.lua
+++ lua/theprimeagen/lazy/toggleterm.lua
@@ -10,13 +10,12 @@ return {
             start_in_insert = true,
             insert_mappings = true, -- whether or not the open mapping applies in insert mode
             persist_size = true,
-            direction = 'horizontal',
+            persist_mode = true, -- if true, the size, position, and enter/insert state will be persisted across sessions
+            direction = 'float',
             close_on_exit = true, -- close the terminal window when the process exits
             shell = vim.o.shell, -- change the default shell
             float_opts = {
                 border = 'single',
-                width = 200,
-                height = 40,
                 winblend = 3,
                 highlights = {
                     border = "Normal",
