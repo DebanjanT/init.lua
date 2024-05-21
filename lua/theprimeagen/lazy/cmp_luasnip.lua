d846a67fa9ed796f6622f1b2a3a4899f5722038a
Debanjan Tewary <89004599+DebanjanT@users.noreply.github.com>
2024-05-21 15:47:05 +0530

    added snip

---
+++ lua/theprimeagen/lazy/cmp_luasnip.lua
@@ -0,0 +1,25 @@
+return {
+	"saadparwaiz1/cmp_luasnip",
+	requires = {
+		"L3MON4D3/LuaSnip",
+		"rafamadriz/friendly-snippets",
+	},
+	config = function()
+		local cmp = require("cmp")
+		local luasnip = require("luasnip")
+
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					require("luasnip").lsp_expand(args.body)
+				end,
+			},
+			sources = {
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" },
+				{ name = "buffer" },
+				{ name = "path" },
+			},
+		})
+	end,
+}
