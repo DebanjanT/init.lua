f5e47f8288bb3fbb28f66ef09b8c8fba48aa8af8
Debanjan Tewary <89004599+DebanjanT@users.noreply.github.com>
2024-06-07 14:31:20 +0530

    feat(main): Added Rose Pine

--- lua/theprimeagen/lazy/colors.lua
+++ lua/theprimeagen/lazy/colors.lua
@@ -1,43 +1,83 @@
 return {
     {
-        "sainnhe/gruvbox-material",
-        config = function()
-            vim.g.gruvbox_material_enable_italic = true
-            vim.g.gruvbox_material_background = "hard"
-        end,
-    },
-    {
         "mellow-theme/mellow.nvim",
         config = function()
-            vim.g.mellow_italic = true
-            vim.g.mellow_contrast = true
-            vim.g.mellow_style = "light"
-            vim.g.mellow_disable_background = false
-            vim.g.mellow_disable_end_of_buffer = true
-            vim.g.mellow_disable_statusline = true
-            vim.g.mellow_disable_italic_comment = true
-            vim.g.mellow_disable_italic_keyword = true
-            vim.g.mellow_disable_italic_function = true
-            vim.g.mellow_disable_italic_variable = true
-            vim.g.mellow_disable_italic_string = true
-            vim.g.mellow_disable_italic_boolean = true
-            vim.g.mellow_disable_italic_number = true
-            vim.g.mellow_disable_italic_constant = true
-            vim.g.mellow_disable_italic_class = true
-            vim.g.mellow_disable_italic_operator = true
-            vim.g.mellow_disable_italic_exception = true
-            vim.g.mellow_disable_italic_parameter = true
-            vim.g.mellow_disable_italic_builtin = true
-           vim.cmd("colorscheme mellow")
+            vim.g.mellow_bold_functions = true
+            vim.g.mellow_italic_functions = true
+            vim.g.mellow_bold_booleans = true
         end,
     },
+    { "folke/tokyonight.nvim", lazy = false, priority = 1000, opts = {} },
     {
-        "crispybaccoon/aki",
+        "rose-pine/neovim",
         config = function()
-            require("aki").setup({
-                transparent_background = true,
-                contrast_dark = "hard", -- 'hard'|'medium'|'soft'
+            require("rose-pine").setup({
+                variant = "auto", -- auto, main, moon, or dawn
+                dark_variant = "main", -- main, moon, or dawn
+                dim_inactive_windows = false,
+                extend_background_behind_borders = true,
+
+                enable = {
+                    terminal = true,
+                    legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
+                    migrations = true, -- Handle deprecated options automatically
+                },
+
+                styles = {
+                    bold = true,
+                    italic = true,
+                    transparency = false,
+                },
+
+                groups = {
+                    border = "muted",
+                    link = "iris",
+                    panel = "surface",
+
+                    error = "love",
+                    hint = "iris",
+                    info = "foam",
+                    note = "pine",
+                    todo = "rose",
+                    warn = "gold",
+
+                    git_add = "foam",
+                    git_change = "rose",
+                    git_delete = "love",
+                    git_dirty = "rose",
+                    git_ignore = "muted",
+                    git_merge = "iris",
+                    git_rename = "pine",
+                    git_stage = "iris",
+                    git_text = "rose",
+                    git_untracked = "subtle",
+
+                    h1 = "iris",
+                    h2 = "foam",
+                    h3 = "rose",
+                    h4 = "gold",
+                    h5 = "pine",
+                    h6 = "foam",
+                },
+
+                highlight_groups = {
+                    -- Comment = { fg = "foam" },
+                    -- VertSplit = { fg = "muted", bg = "muted" },
+                },
+
+                before_highlight = function(group, highlight, palette)
+                    -- Disable all undercurls
+                    -- if highlight.undercurl then
+                    --     highlight.undercurl = false
+                    -- end
+                    --
+                    -- Change palette colour
+                    -- if highlight.fg == palette.pine then
+                    --     highlight.fg = palette.foam
+                    -- end
+                end,
             })
+            vim.cmd("colorscheme rose-pine")
         end,
     },
 }
